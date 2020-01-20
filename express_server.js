943a1eea105614a7c61c058ff08c0a00bf76a31d
kzcheng <kevinzifancheng@gmail.com>
2020-01-20 15:05:00 -0800

    Added route /urls.json

--- express_server.js
+++ express_server.js
@@ -7,8 +7,12 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+// Routes
 app.get("/", (req, res) => {
   res.send("Hello!");
+});
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
 });
 
 app.listen(PORT, () => {
