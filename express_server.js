901551e7888c2f03d32c1ab1f369b27354fe3a1f
kzcheng <kevinzifancheng@gmail.com>
2020-01-20 16:26:34 -0800

    Added some comments. Also set ejs as the view engine.

--- express_server.js
+++ express_server.js
@@ -1,13 +1,20 @@
+// -Constants-
+const PORT = 8080;
+
+// -Imports-
 const express = require("express");
 const app = express();
-const PORT = 8080; // default port 8080
 
+// Set ejs as the view engine
+app.set("view engine", "ejs");
+
+// -Temporary URL Database-
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-// Routes
+// -Routes-
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -18,6 +25,7 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+// Listen
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
