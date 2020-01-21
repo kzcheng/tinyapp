b669e04c520c44cd1216d90ff1d6e19b671f2006
kzcheng <kevinzifancheng@gmail.com>
2020-01-21 12:50:22 -0800

    You can now create new url pairs and save them in memory. Also redirects works.

--- express_server.js
+++ express_server.js
@@ -3,10 +3,11 @@ const PORT = 8080;
 
 // -Imports-
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
+app.set("view engine", "ejs"); // Set ejs as the view engine
+app.use(bodyParser.urlencoded({extended: true}));
 
-// Set ejs as the view engine
-app.set("view engine", "ejs");
 
 // -Temporary URL Database-
 const urlDatabase = {
@@ -14,7 +15,19 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-// -Routes-
+// -Useful Functions-
+const generateRandomString = function() {
+  const length = 6;
+  let result = '';
+  let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let charactersLength = characters.length;
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+};
+
+// -Get-
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -30,9 +43,25 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
+});
+
+// -Post-
+app.post("/urls", (req, res) => {
+  // console.log(req.body);  // Log the POST request body to the console
+
+  const shortURL = generateRandomString();
+  urlDatabase[shortURL] = req.body.longURL;
+  console.log(urlDatabase);
+
+  // res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  res.redirect(`/urls/${shortURL}`);
 });
 
 // Listen
