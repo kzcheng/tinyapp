f990d76a085b1519d9a35f7390c4992c4cf6b989
kzcheng <kevinzifancheng@gmail.com>
2020-01-21 17:35:31 -0800

    Added a login interface on the top right of the webpage. It's almost functioning, but there are no code in .js file to deal with it yet. Also, I changed all quotes to backticks, because I prefer it more. Except in urlDatabase's keys. For some reason the keys aren't compatible with backticks.

--- express_server.js
+++ express_server.js
@@ -2,28 +2,28 @@
 const PORT = 8080;
 
 // -Imports-
-const express = require("express");
-const bodyParser = require("body-parser");
-const cookieParser = require('cookie-parser');
+const express = require(`express`);
+const bodyParser = require(`body-parser`);
+const cookieParser = require(`cookie-parser`);
 
 // -App-
 const app = express();
-app.set("view engine", "ejs"); // Set ejs as the view engine
+app.set(`view engine`, `ejs`); // Set ejs as the view engine
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
 
 
 // -Temporary URL Database-
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "b2xVn2": 'http://www.lighthouselabs.ca',
+  "9sm5xK": 'http://www.google.com'
 };
 
 // -Useful Functions-
 const generateRandomString = function() {
   const length = 6;
-  let result = '';
-  let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let result = ``;
+  let characters = `ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789`;
   let charactersLength = characters.length;
   for (let i = 0; i < length; i++) {
     result += characters.charAt(Math.floor(Math.random() * charactersLength));
@@ -32,39 +32,41 @@ const generateRandomString = function() {
 };
 
 // -Get-
-app.get("/", (req, res) => {
-  res.send("Hello!");
+app.get(`/`, (req, res) => {
+  res.send(`Hello!`);
 });
 
-app.get("/urls.json", (req, res) => {
+app.get(`/urls.json`, (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+app.get(`/hello`, (req, res) => {
+  res.send(`<html><body>Hello <b>World</b></body></html>\n`);
 });
 
-app.get("/urls", (req, res) => {
+app.get(`/urls`, (req, res) => {
   let templateVars = {urls: urlDatabase};
-  res.render("urls_index", templateVars);
+  res.render(`urls_index`, templateVars);
 });
 
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+app.get(`/urls/new`, (req, res) => {
+  res.render(`urls_new`);
 });
 
-app.get("/urls/:shortURL", (req, res) => {
+app.get(`/urls/:shortURL`, (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
-  res.render("urls_show", templateVars);
+  res.render(`urls_show`, templateVars);
 });
 
-app.get("/u/:shortURL", (req, res) => {
+app.get(`/u/:shortURL`, (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
 });
 
 // -Post-
-app.post("/urls", (req, res) => {
+app.post(`/login`,);
+
+app.post(`/urls`, (req, res) => {
   // console.log(req.body);  // Log the POST request body to the console
 
   const shortURL = generateRandomString();
